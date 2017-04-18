4ae8eeeb539b225dcd4dd2f38f3ad2c006b8af36
elson Huang <elson.huang@jamzoo.com.tw>
2017-04-19 07:32:12 +0800

    手動加入scrollView跟tableview及uirefresh更新元件

--- horizontalMenu/ViewController.swift
+++ horizontalMenu/ViewController.swift
@@ -8,16 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    
+    var myrefreshcontrol = UIRefreshControl()
+    @IBOutlet weak var mytableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mytableview.dataSource = self
+        mytableview.delegate = self
+        
+        //出現分隔線
+        let frame = CGRect(x: 0, y: 0, width: self.mytableview.frame.size.width, height: 1/UIScreen.main.scale)
+        let line:UIView = UIView(frame: frame)
+        self.mytableview.tableHeaderView = line
+        line.backgroundColor = self.mytableview.separatorColor
+        
+        //下拉更新元件
+        myrefreshcontrol.backgroundColor = UIColor.clear
+        myrefreshcontrol.tintColor = UIColor.black
+        myrefreshcontrol.attributedTitle = NSAttributedString(string: "更新資料")
+        myrefreshcontrol.addTarget(self, action: #selector(loadData), for: .valueChanged)
+        self.mytableview.addSubview(myrefreshcontrol)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadData(){
+        print("下載資料中")
+        //下載完後
+        if self.myrefreshcontrol.isRefreshing {
+            self.myrefreshcontrol.endRefreshing()
+        }
+    }
+    
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = mytableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "測試\(indexPath.row)"
+        return cell
     }
 
 
