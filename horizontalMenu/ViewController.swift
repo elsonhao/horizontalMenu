da2c5221d244b841295dac426becdb97f12af27a
elson Huang <elson.huang@jamzoo.com.tw>
2017-04-27 07:20:49 +0800

    使用 scroll view 元件完成滑動選單 ,並能夠切換水平頁面

--- horizontalMenu/ViewController.swift
+++ horizontalMenu/ViewController.swift
@@ -8,34 +8,121 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+class ViewController: UIViewController {
 
+    @IBOutlet weak var page1Button: UIButton!
+    @IBOutlet weak var page2Button: UIButton!
+    @IBOutlet weak var page3Button: UIButton!
+    @IBOutlet weak var page4Button: UIButton!
+    @IBOutlet weak var page5Button: UIButton!
     
+    var selectedButton:UIButton!
+
+    var page1ViewController:Page1TableViewController!
+    lazy var page2ViewController:Page2ViewController = {
+        self.storyboard?.instantiateViewController(withIdentifier: "Page2") as! Page2ViewController
+    }()
+    lazy var page3ViewController:Page3ViewController = {
+        self.storyboard?.instantiateViewController(withIdentifier: "Page3") as! Page3ViewController
+    }()
+    lazy var page4ViewController:Page4ViewController = {
+        self.storyboard?.instantiateViewController(withIdentifier: "Page4") as! Page4ViewController
+    }()
+    lazy var  page5ViewController:Page5ViewController = {
+        self.storyboard?.instantiateViewController(withIdentifier: "Page5") as! Page5ViewController
+    }()
+    
+    var selectedViewController:UIViewController!
+    
+    @IBOutlet weak var myScrollView: UIScrollView!
+    
+    @IBOutlet weak var containerView: UIView!
+    //新增成員變數
     var myrefreshcontrol = UIRefreshControl()
-    @IBOutlet weak var mytableview: UITableView!
+    
+    @IBAction func showPage1(_ sender: Any) {
+        changeTab(to: page1Button)
+        changePage(to: page1ViewController)
+    }
+    @IBAction func showPage2(_ sender: Any) {
+        changeTab(to: page2Button)
+        changePage(to: page2ViewController)
+    }
+    @IBAction func showPage3(_ sender: Any) {
+        changeTab(to: page3Button)
+        changePage(to: page3ViewController)
+    }
+    @IBAction func showPage4(_ sender: Any) {
+        changeTab(to: page4Button)
+        changePage(to: page4ViewController)
+    }
+    @IBAction func showPage5(_ sender: Any) {
+        changeTab(to: page5Button)
+        changePage(to: page5ViewController)
+    }
+    
+    func changeTab(to newbutton:UIButton){
+        let defaultColor = selectedButton.tintColor
+        selectedButton.backgroundColor = UIColor.white
+        selectedButton.setTitleColor(defaultColor, for: .normal)
+        
+        newbutton.backgroundColor = UIColor.lightGray
+        newbutton.setTitleColor(UIColor.black, for: .normal)
+        
+        selectedButton = newbutton
+        
+       
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ContainerViewSegue" {
+            page1ViewController = segue.destination as! Page1TableViewController
+            
+        }
+    }
+    
+    func changePage(to newViewController:UIViewController){
+        selectedViewController.willMove(toParentViewController: nil)
+        selectedViewController.view.removeFromSuperview()
+        selectedViewController.removeFromParentViewController()
+        
+        addChildViewController(newViewController)
+        self.containerView.addSubview(newViewController.view)
+        newViewController.view.frame = containerView.bounds
+        newViewController.didMove(toParentViewController: self)
+        
+        self.selectedViewController = newViewController
+    }
+    
+    
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        mytableview.dataSource = self
-        mytableview.delegate = self
+        myScrollView.scrollsToTop = false
         
-        //出現分隔線
-        let frame = CGRect(x: 0, y: 0, width: self.mytableview.frame.size.width, height: 1/UIScreen.main.scale)
-        let line:UIView = UIView(frame: frame)
-        self.mytableview.tableHeaderView = line
-        line.backgroundColor = self.mytableview.separatorColor
+        selectedViewController = page1ViewController
+      
         
-        //下拉更新元件
-        myrefreshcontrol.backgroundColor = UIColor.clear
-        myrefreshcontrol.tintColor = UIColor.black
-        myrefreshcontrol.attributedTitle = NSAttributedString(string: "更新資料")
-        myrefreshcontrol.addTarget(self, action: #selector(loadData), for: .valueChanged)
-        self.mytableview.addSubview(myrefreshcontrol)
+        
+        
+        
+        selectedButton = page1Button
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    
+    
+    
     
     func loadData(){
         print("下載資料中")
@@ -56,11 +143,11 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         return 20
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = mytableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "測試\(indexPath.row)"
-        return cell
-    }
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = mytableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = "測試\(indexPath.row)"
+//        return cell
+//    }
 
 
 }
